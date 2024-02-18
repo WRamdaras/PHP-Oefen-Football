97afad7098b509564f720913c7c9344478ee6086
WRamdaras <112637524+WRamdaras@users.noreply.github.com>
2024-02-18 16:29:14 +0100

    implented tailwind

--- src/Controller/GuestController.php
+++ src/Controller/GuestController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class GuestController extends AbstractController
 {
-    #[Route('/', name: 'app_guest')]
+    #[Route('/guest ', name: 'app_guest')]
     public function index(): Response
     {
         return $this->render('guest/index.html.twig', [
